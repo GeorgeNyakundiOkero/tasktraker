1217a26402151eeb974ef15889ddc6e18202becd
GEORGENYAKUNDIOKERO <georgenyakundio871@gmail.com>
2022-06-10 12:08:52 +0300

    Complete CRUD functionality added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
-const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB  = require('./config/db.js')
+const port = process.env.PORT || 5000
 
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
